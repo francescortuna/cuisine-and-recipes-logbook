04a6bade683f1f62d1b66cb798384c6bfd6ed677
Sophia <marousiss@gmail.com>
2022-11-22 19:41:05 -0500

    updated server.js and controller/api/ndex.js

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const session = require("express-session");
 const routes = require("./controllers");
 const exphbs = require("express-handlebars");
 const sequelize = require("./config/connection");
-const helpers = require("./utils/helpers");
+
+//const helpers = require("./utils/helpers");
 
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
@@ -12,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({});
 
 const sess = {
   secret: "Super secret secret",
